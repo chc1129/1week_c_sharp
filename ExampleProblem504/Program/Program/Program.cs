9db2ee4fe527e397fe3ea8fa405b95471a06cd54
chc1129 <chc1129@gmail.com>
2022-04-16 19:15:26 +0900

    cs prg fix

---
+++ ExampleProblem504/Program/Program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleProbrem504
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // インスタンス生成
+            Ship ship = new Ship();
+            BattleShip battleShip = new BattleShip();
+            // 航行する
+            ship.Sail();
+            battleShip.Sail();
+            // 戦闘する
+            battleShip.Fight();
+        }
+    }
+}
